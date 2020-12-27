d49e32dcde4adb6e6bcf2699aa345a196a0a9c11
Leonardo-Antonio <leo2001.nl8@gmail.com>
2020-12-27 12:41:38 -0500

    change of url

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ list.style.display = 'none';
 form.addEventListener('submit', async (e) => {
     e.preventDefault()
     const fd = new FormData(form);
-    const response = await fetch("http://localhost:8080/api/v1/image/encrypt", {
+    const response = await fetch("https://api-convert-to-b64.herokuapp.com/api/v1/image/encrypt", {
         method: 'POST',
         body: fd,
     });
