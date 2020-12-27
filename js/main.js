3862287fdcd4b70fcea705a048e5ba4fcdf8230c
Leonardo-Antonio <leo2001.nl8@gmail.com>
2020-12-26 20:52:21 -0500

    convert b64 files complete

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const form = document.getElementById('form');
 form.addEventListener('submit', async (e) => {
     e.preventDefault()
     const fd = new FormData(form);
-    const response = await fetch("http://localhost:8080/api/v1/image/encrypt", {
+    const response = await fetch("https://api-convert-to-b64.herokuapp.com/api/v1/image/encrypt", {
         method: 'POST',
         body: fd,
     });
