800e486218addc12aab7bbc4246586715c2b4faf
Leonardo-Antonio <leo2001.nl8@gmail.com>
2020-12-27 12:38:10 -0500

    file_name and size

--- js/main.js
+++ js/main.js
@@ -1,13 +1,23 @@
 const form = document.getElementById('form');
-
+const list = document.getElementById('list');
+list.style.display = 'none';
 form.addEventListener('submit', async (e) => {
     e.preventDefault()
     const fd = new FormData(form);
-    const response = await fetch("https://api-convert-to-b64.herokuapp.com/api/v1/image/encrypt", {
+    const response = await fetch("http://localhost:8080/api/v1/image/encrypt", {
         method: 'POST',
         body: fd,
     });
-    const image = await response.json();
+    if (!response.ok)
+        return;
+    const image = await response.json()
+
+    document.getElementById('file_name').
+        textContent += image.data.file_name;
+    document.getElementById('size').
+        textContent += image.data.size;
+
+    list.style.display = 'block';
     document.getElementById('response')
-        .value = image.data
+        .value = image.data.src
 })
